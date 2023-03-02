b28aed03d645e9c96d76f07b88543f250fdddff7
msoukkar <126202493+msoukkar@users.noreply.github.com>
2023-03-02 18:50:30 -0500

    Add files via upload

---
+++ Week5porfolio5/src/app/mock-heroes.ts
@@ -0,0 +1,20 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'M. Soukkarieh' },
+  { id: 13, name: 'M. Soukkarieh ' },
+  { id: 14, name: 'M. Soukkarieh' },
+  { id: 15, name: 'M. Soukkarieh' },
+  { id: 16, name: 'M. Soukkarieh' },
+  { id: 17, name: 'M. Soukkarieh' },
+  { id: 18, name: 'M. Soukkarieh' },
+  { id: 19, name: 'M. Soukkarieh' },
+  { id: 20, name: 'M. Soukkarieh' }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
